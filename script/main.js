ad237b3bd2e880b66057bd0abc0c18b8a98d266f
Roman Krewer <roman.krewer@deutsche-boerse.com>
2016-02-17 15:36:21 +0100

    draw a line

--- script/main.js
+++ script/main.js
@@ -1,3 +1,21 @@
+var drawingCanvas;
+var draw;
+var maxx = 1000;
+var maxy = 800;
+
 function test() {
- 
+	draw.beginPath();
+	draw.moveTo(0,0);
+	draw.lineTo(maxx,maxy);
+	draw.stroke(); 
+}
+
+function main() {
+	drawingCanvas = document.getElementById('canvascontainer');
+	if(drawingCanvas.getContext) {
+		draw = drawingCanvas.getContext('2d');
+	}
+
+	test();
+
 }
